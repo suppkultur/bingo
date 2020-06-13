a18e8a0100dc5807a463dc2fb0f079372dbcd69f
Arne Bahlo <hallo@arne.me>
2020-06-13 18:21:04 +0200

    Return PDF from function

--- .netlify/functions/generate-bingo-sheet.js
+++ .netlify/functions/generate-bingo-sheet.js
@@ -1,6 +1,32 @@
+const PDFDocument = require('pdfkit');
+
+async function generatePDF() {
+    return new Promise((resolve, reject) => {
+        let doc = new PDFDocument;
+
+        // Collect data and resolve promise on success.
+        let buffers = [];
+        doc.on('data', (buffer) => buffers.push(buffer))
+        doc.on('end', () => {
+            console.log(buffers)
+            let buffer = Buffer.concat(buffers);
+            resolve(buffer)
+        })
+
+        doc.text('Hello world!')
+
+        doc.end();
+    })
+}
+
 exports.handler = async (event, context) => {
+    let buffer = await generatePDF()
     return {
+        isBase64Encoded: true,
         statusCode: 200,
-        body: "Hello, World"
+        headers: {
+            'Content-Type': 'application/pdf',
+        },
+        body: buffer.toString('base64'),
     };
 };
